efa911d8129eb0ef1780a5b4e2e08b4bab89a6d5
Sekou S. Seh <sehsekou98@gmail.comh>
2023-05-27 14:57:52 +0000

    bug fixed

--- src/main/java/com/sekoucode/sekoubookstore/domain/BookNotFoundException.java
+++ src/main/java/com/sekoucode/sekoubookstore/domain/BookNotFoundException.java
@@ -2,6 +2,7 @@ package com.sekoucode.sekoubookstore.domain;
 
 public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException(String isbn) {
+
         super("The book with ISBN " + isbn + " was not found.");
     }
 }
